093529a68ffea20e940bfd5674456293e51d031d
Alvaro Piña <alvapina@ucm.es>
2023-10-20 18:03:11 +0200

    Carpertas organizadas para el proyecto

--- js/escena.js
+++ js/escena.js
@@ -9,14 +9,13 @@ export class LoadScene extends Phaser.Scene
     }
 
     preload() {
-        const image = this.load.image("background", "img/Logo.png");
+        const image = this.load.image("background", "Assets/WebPage/Img/Logo.png");
     }
     create() {
         let image = this.add.image(0, 0, "background");
         image.setOrigin(0.5,0.5);
         image.setPosition(this.game.config.width/ 2, this.game.config.height/ 2);
         image.setScale(0.785,0.785);
-        this.background = image;
         this.loadingText = this.add.text(this.game.config.width / 2, this.game.config.height / 2, "Cargando...", {
             fontFamily: "Arial",
             fontSize: "26px",
