df2d65cf7af67b133c7813fb5825d997c799984f
josepk01 <72452701+josepk01@users.noreply.github.com>
2023-12-16 19:30:54 +0100

    correccion de ataque y movimiento logico

--- js/Objetos/Proyectil.js
+++ js/Objetos/Proyectil.js
@@ -1,14 +1,20 @@
 import LifeComponent from "../Characters/lifeComponent.js";
 
 export default class Proyectil extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene, x, y, texture, dir, damage) {
+    constructor(scene, x, y, texture, velocityVector, damage) {
         super(scene, x, y, texture);
         scene.add.existing(this);
         scene.physics.add.existing(this);
 
-        this.speed = 8;
-        this.setScale(0.05);
-        this.setBounce(0.2);
+        // Definir la velocidad del proyectil
+        this.speed = 700; // Puedes ajustar la velocidad según necesites
+
+        // Establecer la velocidad del proyectil basada en el vector de velocidad
+        this.body.velocity.copy(velocityVector.normalize().scale(this.speed));
+
+        // Configuraciones adicionales
+        this.setScale(0.05); // Ajustar la escala si es necesario
+        this.setBounce(0.2); // Ajustar el rebote si es necesario
         this.setCollideWorldBounds(true);
 
         // Tiempo de vida del proyectil
@@ -16,14 +22,14 @@ export default class Proyectil extends Phaser.Physics.Arcade.Sprite {
 
         // Destruir el proyectil después de su tiempo de vida
         this.setLifeTime(this.lifespan);
-  
+
         // Llama a esta función para manejar la colisión con el suelo.
         this.handleCollisionWithGround();
 
-        //Llama a esta funcion para manejar la colision de los proyectiles con un enemigo de la escena
+        // Llama a esta función para manejar la colisión con enemigos.
         this.handleCollisionWithEnemies();
 
-        this.dir = dir;
+        // Guardar el daño que el proyectil inflige
         this.damage = damage;
     }
 
@@ -36,7 +42,7 @@ export default class Proyectil extends Phaser.Physics.Arcade.Sprite {
 
         // Añadir colisionador con el suelo
         this.scene.physics.add.collider(this, groundLayer, () => {
-            this.destroy(); // Destruye la poción al colisionar con el suelo
+            this.destroy(); // Destruye el proyectil al colisionar con el suelo
         }, null, this);
     }
 
@@ -44,18 +50,20 @@ export default class Proyectil extends Phaser.Physics.Arcade.Sprite {
         this.scene.time.delayedCall(duration, this.destroy, [], this);
     }
 
-    handleCollisionWithEnemies(){
-        //Obtenemos a los enemigos de la escena con getEnemies
+    handleCollisionWithEnemies() {
+        // Obtiene una referencia a los enemigos de la escena
         let enemies = this.scene.getEnemies();
-        this.scene.physics.add.collider(this, enemies, () => {
-            //Daña a los enemigos (aun no tenemos enemigos, por eso lo comento) 
-            //enemies.lifeComp.Damage(this.damage);
-            //Destruimos proyectil
-            this.destroy();
+
+        // Añadir colisionador con los enemigos
+        this.scene.physics.add.collider(this, enemies, (proyectil, enemy) => {
+            enemy.receiveDamage(this.damage); // Suponiendo que los enemigos tienen un método `receiveDamage`
+            this.destroy(); // Destruye el proyectil al colisionar con un enemigo
         });
     }
+
     preUpdate(t, dt) {
         super.preUpdate(t, dt);
-        this.x += this.speed * this.dir;
+
+        // Aquí podrías agregar lógica adicional que se ejecute en cada frame, si es necesario
     }
 }
